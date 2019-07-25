73f7474d49bc774d73440b22fdb35adf2460e4ea
D.Dvali <dvali.daviti@gmail.com>
2019-07-25 16:25:30 +0400

    Add project files.

---
+++ SigningXml.Domain/NbeXmlDocumentSigner.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SigningXml
+{
+    public class NbeXmlDocumentSigner
+    {
+        public static byte[] SignBodyParameter(XmlDocument doc, RSA key)
+        {
+            var responseXml = doc.GetElementsByTagName("xml")[0];
+
+            var docForSign = new XmlDocument();
+            docForSign.LoadXml(responseXml.InnerText);
+
+            if (docForSign.FirstChild.Name == "xml")
+                docForSign.RemoveChild(docForSign.FirstChild);
+
+            var signedXml = new SignedXml
+            {
+                SigningKey = key
+            };
+
+            var objectID = docForSign.FirstChild.Name;
+            var dataObject = new DataObject
+            {
+                Data = docForSign.ChildNodes,
+                Id = objectID
+            };
+
+            signedXml.AddObject(dataObject);
+
+            signedXml.AddReference(new Reference($"#{objectID}"));
+
+            signedXml.ComputeSignature();
+
+            //var realXml = signedXml.GetXml().OuterXml;
+
+            //var el = new XElement("Root", realXml);
+            //responseXml.InnerXml = el.LastNode.ToString();
+
+            //responseXml.InnerXml = System.Web.HttpUtility.HtmlEncode(signedXml.GetXml().OuterXml);
+
+            var s = System.Security.SecurityElement.Escape(signedXml.GetXml().OuterXml);
+
+            
+            responseXml.InnerText = System.Security.SecurityElement.Escape(signedXml.GetXml().OuterXml);
+
+            //new XmlElement()
+
+            //responseXml.InnerXml = signedXml.GetXml().OuterXml.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("&#34;", "&quot;").Replace("'", "&apos;");
+
+            return signedXml.SignatureValue;
+        }
+
+        public static (string signedEnvelope, byte[] signature) SignBodyParameter(string envelope, RSA key)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(envelope);
+            var responseXml = doc.GetElementsByTagName("xml")[0];
+
+            var docForSign = new XmlDocument();
+            docForSign.LoadXml(responseXml.InnerText);
+
+            if (docForSign.FirstChild.Name == "xml")
+                docForSign.RemoveChild(docForSign.FirstChild);
+
+            var signedXml = new SignedXml
+            {
+                SigningKey = key
+            };
+
+            var objectID = docForSign.FirstChild.Name;
+            var dataObject = new DataObject
+            {
+                Data = docForSign.ChildNodes,
+                Id = objectID
+            };
+
+            signedXml.AddObject(dataObject);
+
+            signedXml.AddReference(new Reference($"#{objectID}"));
+
+            signedXml.ComputeSignature();
+
+            var securityElement = SecurityElement.FromString(envelope);
+
+            var k = securityElement.SearchForChildByTag("s:Body").SearchForChildByTag("RegisterResponse").SearchForChildByTag("xml");
+            k.Text = SecurityElement.Escape(signedXml.GetXml().OuterXml);
+
+            return (securityElement.ToString(), signedXml.SignatureValue);
+        }
+
+        public static bool ValidateSignature(XmlDocument xmlDoc, RSA publicKey)
+        {
+            //try
+            //{
+                xmlDoc.PreserveWhitespace = true;
+
+                var signedXmlObj = new SignedXml(xmlDoc);
+
+                var nodeList = xmlDoc.GetElementsByTagName("Signature");
+                signedXmlObj.LoadXml((XmlElement)nodeList[0]);
+
+                return signedXmlObj.CheckSignature(publicKey);
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+        }
+
+        public static RSA GetPrivateKeyFromPath(string privateKeyPath, string privateKeyPassword) => new X509Certificate2(privateKeyPath, privateKeyPassword).GetRSAPrivateKey();
+
+        public static RSA GetPublicKeyFromPath(string publicKeyPath)
+        {
+            var bytesArr = File.ReadAllBytes(publicKeyPath);
+            var collection = new X509Certificate2Collection();
+            collection.Import(bytesArr);
+            //return (RSACryptoServiceProvider)collection[0].PublicKey.Key;//.GetRSAPublicKey();
+            return collection[0].GetRSAPublicKey();
+            //return new X509Certificate2(publicKeyPath).GetRSAPublicKey();
+        }
+    }
+}
---
+++ SigningXml/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SigningXml
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBox4.Text = "Aa123456";
+            //textBox3.Text = @"D:\Projects\NBE-Dev\AltaSoft.Nbe.Processor\bin\Debug\cert.pfx";
+            textBox3.Text = @"C:\Users\d.dvali\Desktop\Aa123456 tsttbcnbe.tbcbank.ge.pfx";
+            textBox5.Text = @"C:\Users\d.dvali\Desktop\certnew.p7b";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(textBox1.Text);
+                var tuple = NbeXmlDocumentSigner.SignBodyParameter(textBox1.Text, NbeXmlDocumentSigner.GetPrivateKeyFromPath(textBox3.Text, textBox4.Text));
+
+                textBox2.Text = tuple.signedEnvelope;
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(textBox2.Text);
+
+                var xmlElement = doc.GetElementsByTagName("xml")[0];
+                var decodedSignature = HttpUtility.HtmlDecode(xmlElement.InnerText);
+
+                //string path = @"D:\Projects\NBE-Dev\AltaSoft.Nbe.Processor\bin\Debug\publickey.cer";
+                //string path = @"C:\Users\d.dvali\Desktop\certnew.p7b";
+                //string path = textBox5.Text;
+
+                var doc2 = new XmlDocument();
+                doc2.LoadXml(decodedSignature);
+
+                if (!checkBox1.Checked)
+                {
+                    if (NbeXmlDocumentSigner.ValidateSignature(doc2,
+                        NbeXmlDocumentSigner.GetPublicKeyFromPath(textBox5.Text)))
+                        MessageBox.Show("CheckedSignature Successful");
+                    else
+                        MessageBox.Show("CheckedSignature not Successful");
+                }
+                else
+                {
+                    if (NbeXmlDocumentSigner.ValidateSignature(doc2,
+                        NbeXmlDocumentSigner.GetPrivateKeyFromPath(textBox3.Text, textBox4.Text))
+                    ) //()))//.GetPrivateKeyFromPath(@"C:\Users\d.dvali\Desktop\Aa123456 tsttbcnbe.tbcbank.ge.pfx", "1")))//(@"C:\Users\d.dvali\Desktop\certnew.p7b")))
+                        MessageBox.Show("CheckedSignature Successful");
+                    else
+                        MessageBox.Show("CheckedSignature not Successful");
+                }
+            }
+
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox5.Text = openFileDialog2.FileName;
+            }
+        }
+    }
+}
